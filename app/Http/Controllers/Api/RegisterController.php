05fd433a02b4c267d398013cf236ba60c5c3fd95
fatima882 <fatimasajjad882@gmail.com>
2024-05-31 02:13:20 +0500

    otp

--- app/Http/Controllers/Api/RegisterController.php
+++ app/Http/Controllers/Api/RegisterController.php
@@ -2,6 +2,7 @@
    
 namespace App\Http\Controllers\API;
    
+use App\Http\Resources\UserResource;
 use Illuminate\Http\Request;
 use App\Http\Controllers\API\BaseController as BaseController;
 use App\Models\User;
@@ -33,10 +34,20 @@ class RegisterController extends BaseController
         $input = $request->all();
         $input['password'] = bcrypt($input['password']);
         $user = User::create($input);
-        $success['token'] =  $user->createToken('MyApp')->plainTextToken;
-        $success['name'] =  $user->name;
+        $token =  $user->createToken('MyApp')->plainTextToken;
+
+        $emailCode = rand(1000, 9999);
+            $user->otp = $emailCode;
+            $user->save();
+            dispatch(new \App\Jobs\EmailVerificationJob($user, $emailCode));
+            return response()->json([
+                'data' => [
+                    'message'=>'otp sended to the email',
+                    'user' => new UserResource($user),
+                    'token' => $token
+                ]
+            ], 200);
    
-        return $this->sendResponse($success, 'User register successfully.');
     }
    
     /**
@@ -48,13 +59,50 @@ class RegisterController extends BaseController
     {
         if(Auth::attempt(['email' => $request->email, 'password' => $request->password])){ 
             $user = Auth::user(); 
-            $success['token'] =  $user->createToken('MyApp')->plainTextToken; 
-            $success['name'] =  $user->name;
+            $token =  $user->createToken('MyApp')->plainTextToken; 
+
+            $emailCode = rand(1000, 9999);
+            $user->otp = $emailCode;
+            $user->save();
+            dispatch(new \App\Jobs\EmailVerificationJob($user, $emailCode));
+
+            return response()->json([
+            'data' => [
+                'user' => new UserResource($user),
+                'activity' => $user->activity ? $user->activity->log : '',
+                'token' => $token
+            ]
+        ], 200);
    
-            return $this->sendResponse($success, 'User login successfully.');
         } 
         else{ 
             return $this->sendError('Unauthorised.', ['error'=>'Unauthorised']);
         } 
+    }
+
+    public function otp(Request $request){
+        
+        $user = User::where('email', $request->email)->first();
+        if($request->otp == $user->otp){
+
+            $user->email_verified_at = now();
+            return response()->json([
+                'data' => [
+                    'message' => 'verified',
+                    'user' => new UserResource($user),
+                ]
+            ], 200);
+
+        }else{
+            return response()->json([
+                'data' => [
+                    'message' => 'invalid otp',
+                    'email' => $request->email,
+                ]
+            ], 204);
+        }
+    
+        
+
     }
 }
